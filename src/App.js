6c47ae88b666e6e02730afe83f47a52e6535d737
Ronald <ronaldodwi.rd@gmail.com>
2024-08-16 20:40:47 +0700

    halo dek

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from "react-particles";
+import { Typewriter } from "react-simple-typewriter";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks"
+import { useState } from "react";
+import Countdown from "react-countdown";
 
 function App() {
+  const [happyIndependenceDay, setHappyIndependaceDay] = useState(["Menjelang", "Peringatan", "Detik-detik", "Proklamasi", "Republik Indonesia"])
+
+  const particleInitialization = async(engine) => {
+    await loadFireworksPreset(engine)
+  }
+
+  function timeLeft() {
+    const independanceDay = new Date("August 17, 2024 10:00:00").getTime()
+    const now = new Date().getTime()
+    const timeRemaining = independanceDay - now
+    return timeRemaining
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles
+        init={particleInitialization}
+        options={{preset: "fireworks"}}
+      />
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <span className="text-red-600 text-4xl font-bold px-4 z-50">
+          <Typewriter 
+            words={happyIndependenceDay} 
+            loop={false}
+            cursorStyle={"_"}
+            cursor
+          />
+        </span>
+        <div className="z-50 text-white">
+          <Countdown date={Date.now() + timeLeft()} onComplete={() => setHappyIndependaceDay([
+            "Dirgahayu", "Republik Indonesia", "Ke-79 🇮🇩"
+          ])}/>
+        </div>
+      </div>
+    </>
   );
 }
 
